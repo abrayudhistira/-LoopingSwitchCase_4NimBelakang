65f78b547ea521e1eb4577606d08dc29a714d4e2
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-03-18 14:50:27 +0700

    Menambahkan program While

--- file2.cpp
+++ file2.cpp
@@ -4,6 +4,22 @@
 using namespace std;
 
 int main (){
+    //Perulangan While
+    cout << "Perulangan While" << endl;
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    srand(time(0));
+    x = rand() % 10;
     
+    cout << "Nilai x awal : " << x << endl;
+    while(x < 7){
+        cout << "Nilai x : " << x << " Lari Keliling Lapangan" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah perulangan : " << perulanganWhile << endl;
+    cout << endl;
 
 }
